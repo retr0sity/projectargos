76073f8324af88829a9f305484ffb1b709cd68ec
Panagiotis Tsompanoglou <panostsomp0211@gmail.com>
2025-12-06 15:16:42 +0200

    Added flappy  basics

--- Assets/ProgrammingTeamAssets/Scripts/Player/Gameplay/MinigamePlayerControllers/FlappyController.cs
+++ Assets/ProgrammingTeamAssets/Scripts/Player/Gameplay/MinigamePlayerControllers/FlappyController.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
 
-public class FlappyController : MonoBehaviour
+public class FlappyController : BasePlayerController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Flight Stats")]
+    [SerializeField] private float autoScrollSpeed = 3f;
+    [SerializeField] private float flapStrength = 5f;
+    [SerializeField] private float maxFallSpeed = 8f;
+
+    protected override void HandleMovement(Vector2 input)
     {
-        
+        // Force auto-scroll X unless locked
+        float xVel = lockAxisX ? 0 : autoScrollSpeed;
+
+        // Clamp falling speed so we don't fall too fast
+        float yVel = Mathf.Clamp(_rb.linearVelocity.y, -maxFallSpeed, maxFallSpeed);
+
+        _rb.linearVelocity = new Vector2(xVel, yVel);
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void HandleJump()
     {
+        // Flap!
+        _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, 0); // Reset Y momentum
+        _rb.AddForce(Vector2.up * flapStrength, ForceMode2D.Impulse);
         
+        _animator.SetTrigger("Flap"); // Make sure you have a Flap trigger
     }
 }
