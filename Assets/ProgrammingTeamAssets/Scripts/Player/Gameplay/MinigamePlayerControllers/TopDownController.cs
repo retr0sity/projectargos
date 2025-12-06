77c4e17cfa2515c058606158ee27564a99cfa414
Panagiotis Tsompanoglou <panostsomp0211@gmail.com>
2025-12-06 15:14:30 +0200

    Added Top down basics

--- Assets/ProgrammingTeamAssets/Scripts/Player/Gameplay/MinigamePlayerControllers/TopDownController.cs
+++ Assets/ProgrammingTeamAssets/Scripts/Player/Gameplay/MinigamePlayerControllers/TopDownController.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
 
-public class TopDownController : MonoBehaviour
+public class TopDownController : BasePlayerController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("TopDown Stats")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    protected override void OnEnable()
     {
-        
+        base.OnEnable();
+        if (_rb != null) _rb.gravityScale = 0f; // Disable gravity
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnDisable()
     {
+        base.OnDisable();
+        if (_rb != null) _rb.gravityScale = 1f; // Re-enable gravity for other modes
+    }
+
+    protected override void HandleMovement(Vector2 input)
+    {
+        // Move in X and Y
+        _rb.linearVelocity = input * moveSpeed;
+
+        // Animation
+        _animator.SetFloat("Speed", input.magnitude);
         
+        // Optional: Add 4-direction facing logic here if you have 4-way sprites
+        if (input.x > 0.01f) transform.localScale = new Vector3(1,1,1);
+        else if (input.x < -0.01f) transform.localScale = new Vector3(-1,1,1);
+    }
+
+    protected override void HandleJump()
+    {
+        // Top down usually doesn't jump, maybe Dash?
+        Debug.Log("Jump/Dash Action");
     }
 }
